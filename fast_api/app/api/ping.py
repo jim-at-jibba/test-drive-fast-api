2d6a7c79658ad6adb925ba269bc48b0aba9a9ea5
James Best <jim@justjibba.net>
2022-07-14 13:25:26 +0100

    Rearrange api code

---
+++ fast_api/app/api/ping.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, Depends
+
+from app.config import get_settings, Settings
+
+router = APIRouter()
+
+
+# Take note of settings: Settings = Depends(get_settings).
+# Here, the Depends function is a dependency that declares another dependency,
+# get_settings. Put another way, Depends depends on the result of get_settings.
+# The value returned, Settings, is then assigned to the settings parameter.
+#
+# FastApi has async built in
+
+
+@router.get("/ping")
+async def pong(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong!",
+        "environment": settings.environment,
+        "testing": settings.testing
+    }
