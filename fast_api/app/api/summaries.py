a9a81f0e34e2ccd651c9f9e711e60bc55df7f9d6
James Best <jim@justjibba.net>
2022-07-19 16:32:46 +0100

    Add remaining endpoints and tests

--- fast_api/app/api/summaries.py
+++ fast_api/app/api/summaries.py
@@ -1,7 +1,9 @@
+from typing import List
 from fastapi import APIRouter, HTTPException
 
 from app.api import crud
 from app.models.pydantic import SummaryPayloadSchema, SummaryPayloadResponse
+from app.models.tortoise import SummarySchema
 
 
 router = APIRouter()
@@ -15,3 +17,17 @@ async def create_summary(payload: SummaryPayloadSchema) -> SummaryPayloadSchema:
         "url": payload.url
     }
     return response_object
+
+
+@router.get("/{id}/", response_model=SummarySchema)
+async def read_summary(id: int) -> SummarySchema:
+    summary = await crud.get(id)
+    if not summary:
+        raise HTTPException(status_code=404, detail="Summary not found")
+
+    return summary
+
+
+@router.get("/", response_model=List[SummarySchema])
+async def read_all_summaries() -> List[SummarySchema]:
+    return await crud.get_all()
